2beeadc67e62a2aa438faf77dc4d432c584f7aee
ArtemStryzhakov <Phernander1@gmail.com>
2022-10-31 22:41:26 +0200

    ....

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const url = "d1cb154147684011afda58be32a45cf9"
-const article = 20
+const article = 15
+const insertData = document.querySelector("#chooseArticle")
+
+insertData.addEventListener("keyup", (event) => {
+    if (event.key == "Enter"){
+        getApiKey()
+    }
+})
+
 function getApiKey() {
-    fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-30&sortBy=publishedAt&apiKey=${url}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.articles)
-            const { title, description, publishedAt, urlToImage } = data.articles[article]
-            const { name } = data.articles[article].source
+        try {
+                fetch(`https://newsapi.org/v2/everything?q=${insertData.value}&from=2022-09-30&sortBy=publishedAt&apiKey=${url}`)
+                    .then((response) => response.json())
+                    .then((data) => {
+                            const { title, description, publishedAt, urlToImage } = data.articles[article]
+                            const { name } = data.articles[article].source
 
-            document.querySelector("#title").innerText = title
-            document.querySelector("#description").innerText = description
+                            document.querySelector("#title").innerText = title;
 
-            const datetime = publishedAt.split("T")
-            document.querySelector("#publishedAt").innerText = datetime[0]
+                            document.querySelector("#description").innerText = description;
 
-            document.querySelector("#urltoImage").src = urlToImage
-            document.querySelector("#urltoImage").style.visibility = "visible"
+                            const datetime = publishedAt.split("T");
+                            document.querySelector("#publishedAt").innerText = datetime[0];
 
-            document.querySelector("#sourcename").innerText = name
-        })
+                            document.querySelector("#urltoImage").src = urlToImage;
+                            document.querySelector("#urltoImage").style.visibility = "visible";
+
+                            document.querySelector("#sourcename").innerText = name;
+                    })
+        } catch (err){
+                console.log("Error")
+        }
 }
-
-getApiKey()
